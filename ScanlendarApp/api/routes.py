3a27b878fc25b38b61a7543cd597491a8ce0671a
Samhith Kakarla <samhith.kakarla@gmail.com>
2021-06-20 03:42:48 -0400

    Setup Scan Route

--- ScanlendarApp/api/routes.py
+++ ScanlendarApp/api/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from .models import Event
 from .serializers import event_schema
 
@@ -8,6 +8,22 @@ scan_api = Blueprint('api', __name__)
 @scan_api.route('/')
 def home():
     return "Hello World"
+
+@scan_api.route('/scan', methods=['POST'])
+def get_calendar_info():
+    text = request.json['text']
+
+    title = ""
+    description = ""
+    start_date = ""
+    end_date = ""
+    start_time = ""
+    end_time = ""
+
+    new_event = Event(title, description, start_date, end_date, start_time, end_time)
+    new_event_data = event_schema.jsonfiy(new_event)
+
+    return new_event_data
 
 
 # Summary, Description, Start (Time), End 
