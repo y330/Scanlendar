931948d04884864bb66056fb393b4a83e95e950f
y330 <avivyonah@gmail.com>
2021-06-20 23:31:17 -0400

    idk if this works but i added attractions librarywith sveltkit and i made it so you can run it with flask but idk

---
+++ ScanlendarApp/Svelte/Frontend/src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script>
+	// your script goes here
+	import { Button } from "../../node_modules/attractions";
+	import Textfield from "../../node_modules/attractions"
+	let eventString = "";
+	let cleanString = "CLick Me"
+</script>
+
+<svelte:head>
+	<script src="https://unpkg.com/attractions"></script>
+</svelte:head>
+
+<Button on:click{() => cleanString = eventString}>{cleanString}</Button>
+<Textfield
+	label="Input calendar event in   natural language"
+	bind:value={eventString}
+	error={eventString.includes("?") && "You confused our NLP model >:("}
+/><h1>Welcome to SvelteKit</h1>
+<a-button filled="filled">My button</a-button>
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<!-- markup (zero or more items) goes here -->
+<style>
+	/* your styles go here */
+
+</style>
---
+++ ScanlendarApp/Svelte/src/routes/index.svelte
@@ -0,0 +1,56 @@
+<script>
+	let clicked = 0;
+	export const prerender = true;
+	// STATE
+	let text = '';
+	let event = {
+		title: '',
+		description: '',
+		location: '',
+		start_date: '',
+		end_date: '',
+		start_time: '',
+		end_time: ''
+	};
+	// API CALLS
+	function getCalendarScan() {
+		// Get event details from text
+	}
+	function formatEventForGoogleCalendar() {
+		// Format event to fit google calendar params
+	}
+	function addToGoogleCalendar() {
+		// Make api call to google calendar to add event
+	}
+	// METHODS
+	const handleTextChange = (e) => {
+		text = e.target.value;
+	};
+
+</script>
+<svelte:head>
+  <title>Home</title>
+</svelte:head>
+
+<section>
+  <h1>
+    <div class="welcome">
+      <picture>
+        <source srcset="scanlendar-transparent.webp" type="image/webp" />
+        <img src="scanlendar-transparent.png" alt="Welcome" />
+      </picture>
+    </div>
+
+    Smart Calendar Tool<br />built with SvelteKit and Django
+  </h1>
+    <p class="mdc-typography--body1">
+    {#if clicked}
+      You've clicked the button {clicked} time{clicked === 1 ? "" : "s"}.
+    {:else}
+      <span class="grayed">You haven't clicked the button.</span>
+    {/if}
+  </p>
+
+  <input on:change={handleTextChange} />
+</section>
+
